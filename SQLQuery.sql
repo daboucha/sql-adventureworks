4ffb383609520364a5e64e8bb5edf50ae5cf310c
Danny <dachakra4@gmail.com>
2022-03-05 14:50:31 -0500

    First update

---
+++ SQLQuery.sql
@@ -0,0 +1,7 @@
+-- List the customers along with their ID numbers. Include the last names, first names, and company names
+SELECT CustomerID, FirstName, LastName, CompanyName
+FROM SalesLT.Customer;
+
+-- list the name, product number, and color of each product
+SELECT Name, ProductNumber, Color
+FROM SalesLT.Product;
